8e8c4cce5f32be952c46cf1e29b444cb2d8f7264
Jmiya808 <156391092+Jmiya808@users.noreply.github.com>
2024-04-18 19:56:38 -0700

    add more stuff

--- components/Header.vue
+++ components/Header.vue
@@ -1,12 +1,15 @@
 <template>
-  <header class="bg-grey-600 py-2">
-    <main class="container mx-auto flex flex-row justify-between"></main>
-    <div class="text-white text-lg font-bold"> Site Name</div>
-    <nav class="flex flex-row justify-end gap-4">
+  <header class="bg-gray-600 py-2">
+    <main class="container mx-auto flex flex-row justify-between">
+      <div class="text-white text-lg font-bold"> 
+         <nuxt-link to="/">Site Name</nuxt-link>
+      </div>
+      <nav class="flex flex-row justify-end gap-4">
       <nuxt-link to="/page1">Page 1</nuxt-link>
       <nuxt-link to="/page2">Page 2</nuxt-link>
       <nuxt-link to="/page3">Page 3</nuxt-link>
       <nuxt-link to="/page4">Page 4</nuxt-link>
-    </nav>
-  </header>>
+      </nav>
+    </main>
+  </header>
 </template>
