38e56178dcd146b46c2976a9109b88b8dff70494
Jmiya808 <156391092+Jmiya808@users.noreply.github.com>
2024-04-18 19:45:52 -0700

    Added new pages

--- components/Header.vue
+++ components/Header.vue
@@ -1,3 +1,12 @@
 <template>
-  Header & Nav here
+  <header class="bg-grey-600 py-2">
+    <main class="container mx-auto flex flex-row justify-between"></main>
+    <div class="text-white text-lg font-bold"> Site Name</div>
+    <nav class="flex flex-row justify-end gap-4">
+      <nuxt-link to="/page1">Page 1</nuxt-link>
+      <nuxt-link to="/page2">Page 2</nuxt-link>
+      <nuxt-link to="/page3">Page 3</nuxt-link>
+      <nuxt-link to="/page4">Page 4</nuxt-link>
+    </nav>
+  </header>>
 </template>
